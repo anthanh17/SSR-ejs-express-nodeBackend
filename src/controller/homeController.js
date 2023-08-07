0fc057ba0da18e7e5c867694ac925ed425c21fa3
Annt <annguyenthe.work@gmail.com>
2023-08-08 00:58:31 +0700

    add upload single file

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,4 @@
+import multer from "multer";
 import pool from "../configs/connectDB";
 
 let getHomepage = async (req, res) => {
@@ -43,6 +44,30 @@ let postUpdateUser = async (req, res) => {
   return res.redirect("/");
 };
 
+let getUploadFile = async (req, res) => {
+  return res.render("uploadFile.ejs");
+};
+
+const upload = multer().single("profile_pic");
+
+let handleUploadFile = async (req, res) => {
+  upload(req, res, function (err) {
+    if (req.fileValidationError) {
+      return res.send(req.fileValidationError);
+    } else if (!req.file) {
+      return res.send("please select an image to upload");
+    } else if (err instanceof multer.MulterError) {
+      return res.send(err);
+    } else if (err) {
+      return res.send(err);
+    }
+
+    res.send(
+      `You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`
+    );
+  });
+};
+
 module.exports = {
   getHomepage,
   getDetailPage,
@@ -50,4 +75,6 @@ module.exports = {
   deleteUser,
   getEditUser,
   postUpdateUser,
+  getUploadFile,
+  handleUploadFile,
 };
